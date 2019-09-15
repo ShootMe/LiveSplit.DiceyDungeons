83a60ff1dfba91f305172330c3850791c3e69c71
ShootMe <birreller@gmail.com>
2019-09-15 11:31:17 -0500

    Performance

--- SplitterMemory.cs
+++ SplitterMemory.cs
@@ -10,54 +10,56 @@ namespace LiveSplit.DiceyDungeons {
 		public Process Program { get; set; }
 		public bool IsHooked { get; set; } = false;
 		public DateTime LastHooked;
+		public IntPtr BaseAddress { get; set; }
 
 		public SplitterMemory() {
 			LastHooked = DateTime.MinValue;
+			BaseAddress = IntPtr.Zero;
 		}
 		public bool HasPointer(Player player = Player.User) {
-			return Program.Read<ulong>(Program.MainModule.BaseAddress, (int)player) != 0;
+			return Program.Read<ulong>(BaseAddress, (int)player) != 0;
 		}
 		public int HP(Player player = Player.User) {
-			return Program.Read<int>(Program.MainModule.BaseAddress, (int)player, 0x1b0);
+			return Program.Read<int>(BaseAddress, (int)player, 0x1b0);
 		}
 		public int MaxHP(Player player = Player.User) {
-			return Program.Read<int>(Program.MainModule.BaseAddress, (int)player, 0x1b4);
+			return Program.Read<int>(BaseAddress, (int)player, 0x1b4);
 		}
 		public int Dice(Player player = Player.User) {
-			return Program.Read<int>(Program.MainModule.BaseAddress, (int)player, 0x190);
+			return Program.Read<int>(BaseAddress, (int)player, 0x190);
 		}
 		public int ActionsThisTurn(Player player = Player.User) {
-			return Program.Read<int>(Program.MainModule.BaseAddress, (int)player, 0x1e0);
+			return Program.Read<int>(BaseAddress, (int)player, 0x1e0);
 		}
 		public int Kills(Player player = Player.User) {
-			return Program.Read<int>(Program.MainModule.BaseAddress, (int)player, 0x1c4);
+			return Program.Read<int>(BaseAddress, (int)player, 0x1c4);
 		}
 		public int Special(Player player = Player.User) {
-			return Program.Read<int>(Program.MainModule.BaseAddress, (int)player, 0x200);
+			return Program.Read<int>(BaseAddress, (int)player, 0x200);
 		}
 		public int SpecialValue(Player player = Player.User) {
-			return Program.Read<int>(Program.MainModule.BaseAddress, (int)player, 0x204);
+			return Program.Read<int>(BaseAddress, (int)player, 0x204);
 		}
 		public int Level(Player player = Player.User) {
-			return Program.Read<int>(Program.MainModule.BaseAddress, (int)player, 0x1c0);
+			return Program.Read<int>(BaseAddress, (int)player, 0x1c0);
 		}
 		public string Name(Player player = Player.User) {
-			return Program.ReadAscii((IntPtr)Program.Read<ulong>(Program.MainModule.BaseAddress, (int)player, 0x68));
+			return Program.ReadAscii((IntPtr)Program.Read<ulong>(BaseAddress, (int)player, 0x68));
 		}
 		public int Gold(Player player = Player.User) {
-			return Program.Read<int>(Program.MainModule.BaseAddress, (int)player, 0x1cc);
+			return Program.Read<int>(BaseAddress, (int)player, 0x1cc);
 		}
 		public bool Turn(Player player = Player.User) {
-			return !Program.Read<bool>(Program.MainModule.BaseAddress, (int)player, 0x20);
+			return !Program.Read<bool>(BaseAddress, (int)player, 0x20);
 		}
 		public int PlayerXP() {
-			return PlayerXPNeeded() - Program.Read<int>(Program.MainModule.BaseAddress, 0x1b1a908);
+			return PlayerXPNeeded() - Program.Read<int>(BaseAddress, 0x1b1a908);
 		}
 		public int PlayerXPNeeded() {
-			return Program.Read<int>(Program.MainModule.BaseAddress, 0x1b1a90c);
+			return Program.Read<int>(BaseAddress, 0x1b1a90c);
 		}
 		public int Floor() {
-			return Program.Read<int>(Program.MainModule.BaseAddress, 0x1b25ecc) + 1;
+			return Program.Read<int>(BaseAddress, 0x1b25ecc) + 1;
 		}
 		public bool HookProcess() {
 			IsHooked = Program != null && !Program.HasExited;
@@ -68,6 +70,7 @@ namespace LiveSplit.DiceyDungeons {
 
 				if (Program != null && !Program.HasExited) {
 					MemoryReader.Update64Bit(Program);
+					BaseAddress = Program.MainModule.BaseAddress;
 					IsHooked = true;
 				}
 			}
